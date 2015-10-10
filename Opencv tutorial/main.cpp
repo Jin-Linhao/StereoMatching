e1600e373ee5477f9f842a7ea06be9b3e81b796b
Jin-Linhao <jin_linhao@hotmail.com>
2015-10-10 14:57:27 +0800

    Initial Commit

---
+++ Opencv tutorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Opencv tutorial
+//
+//  Created by Linhao Jin on 10/10/15.
+//  Copyright © 2015 Linhao Jin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
